ef6fcef2d59db00df016b91e4a4e4e64a3cf723e
Your Name <vic@lematec.co>
2026-05-25 08:42:13 +0800

    Add PWA home screen support

---
+++ lematec-erp/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'lematec-erp-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const responseCopy = response.clone();
+        if (new URL(request.url).origin === self.location.origin) {
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, responseCopy));
+        }
+        return response;
+      })
+      .catch(() => {
+        if (request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+        return caches.match(request);
+      })
+  );
+});
